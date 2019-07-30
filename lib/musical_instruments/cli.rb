46ec04a049a448ca9a7532861aae23c9f44dcd84
David Foote <david.a.foote@gmail.com>
2019-07-30 18:16:39 +0000

    Stubbing out more of the interface in preparation for real data

--- lib/musical_instruments/cli.rb
+++ lib/musical_instruments/cli.rb
@@ -2,17 +2,42 @@
 class MusicalInstruments::CLI
   
   def call
-    puts "Musical instruments for sale:"
+    puts "Welcome to My-Music!"
     list_instruments
+    catalog
+    next_time
   end
   
   def list_instruments
+    puts "Musical instruments for sale:"
     puts <<-DOC.gsub /^\s*/, ''
-    Roland RD-2000 88-key Stage Piano, $2399.99
-    Fender American Elite V Jazz Bass, 5-String (Maple, with Case), Natural, $2149.99
-    Maharaja Tabla Drum Set- Buy 3KG Black Brass Bayan, Finest Dayan with Book, Hammer, Cushions and Cover (PDI-EA)- $285.00
-    Cremona SV-500 Series Violin Outfit, $399.99
+    1. Roland RD-2000 88-key Stage Piano, $2399.99
+    2. Fender American Elite V Jazz Bass, 5-String (Maple, with Case), Natural, $2149.99
+    3. Maharaja Tabla Drum Set- Buy 3KG Black Brass Bayan, Finest Dayan with Book, Hammer, Cushions and Cover (PDI-EA)- $285.00
+    4. Cremona SV-500 Series Violin Outfit, $399.99
     DOC
+  end
+  
+  def catalog
+    input = nil
+    while input != "exit"
+      puts "Enter the number of the instrument for a description or type exit:"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "Description for instrument 1..."
+      when "2"
+        puts "Description for instrument 2..."
+      when "3"
+        puts "Description for instrument 3..."
+      when "4"
+        puts "Description for instrument 4..."
+      end
+    end
+  end
+  
+  def next_time
+    puts "See you next time and thanks for stopping by!"
   end
 end
 
