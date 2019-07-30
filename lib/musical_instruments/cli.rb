4cf8ac96e31814b03badcd8887f524bde8c2b7cf
David Foote <david.a.foote@gmail.com>
2019-07-30 17:34:52 +0000

    Stubbing out the interface w/ fake data for now

--- lib/musical_instruments/cli.rb
+++ lib/musical_instruments/cli.rb
@@ -3,5 +3,16 @@ class MusicalInstruments::CLI
   
   def call
     puts "Musical instruments for sale:"
+    list_instruments
+  end
+  
+  def list_instruments
+    puts <<-DOC
+    Roland RD-2000 88-key Stage Piano, $2399.99
+    Fender American Elite V Jazz Bass, 5-String (Maple, with Case), Natural, $2149.99
+    Maharaja Tabla Drum Set- Buy 3KG Black Brass Bayan, Finest Dayan with Book, Hammer, Cushions and Cover (PDI-EA)- $285.00
+    Cremona SV-500 Series Violin Outfit, $399.99
+    DOC
   end
 end
+
