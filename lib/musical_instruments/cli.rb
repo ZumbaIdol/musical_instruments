b9451654a5ab86ed2ca643cc24de2eb593e1b9fe
David Foote <david.a.foote@gmail.com>
2019-08-01 22:44:42 +0000

    Made adjustments to the cli

--- lib/musical_instruments/cli.rb
+++ lib/musical_instruments/cli.rb
@@ -11,19 +11,21 @@ class MusicalInstruments::CLI
     puts "Musical Instruments for sale:"
     @instruments = MusicalInstruments::Instruments.cataolog
     @instruments.each.with_index(1) do |instrument, i|
-      puts "#{i}. #{instrument.name} - #{instrument.price} - #{instrument.description}"
+      puts "#{i}. #{instrument.name} - #{instrument.price}"
     end
   end
   
   def catalog
     input = nil
     while input != "exit"
-      puts "Enter the number of the instrument for a description, cat to see the catalog again, or type exit:"
+      puts "Enter the number of the instrument you would like more info about, descrip to see a description, cat to see the catalog again, or type exit:"
       input = gets.strip.downcase
       
       if input.to_i > 0
         the_instrument = @instruments[input.to_i - 1]
-        puts "#{the_instrument.name} - #{the_instrument.price} - #{the_instrument.description}"
+        puts "#{the_instrument.name} - #{the_instrument.price}"
+      elsif input == "descrip"
+        puts "#{the_instrument.description}"
       elsif input == "cat"
         list_instruments
       elsif input == "exit"
