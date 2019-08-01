a2c3f932ade5f4812236107effb76b86dc694d7a
David Foote <david.a.foote@gmail.com>
2019-08-01 23:00:27 +0000

    Getting closer...error message not working as expected

--- lib/musical_instruments/cli.rb
+++ lib/musical_instruments/cli.rb
@@ -18,26 +18,27 @@ class MusicalInstruments::CLI
   def catalog
     input = nil
     while input != "exit"
-      puts "Enter the number of the instrument you would like more info about, descrip to see a description, cat to see the catalog again, or type exit:"
+      puts "Enter the number of the instrument you would like more info about, cat to see the catalog again, or type exit:"
       input = gets.strip.downcase
       
       if input.to_i > 0
         the_instrument = @instruments[input.to_i - 1]
         puts "#{the_instrument.name} - #{the_instrument.price}"
+        puts "For more info, type descrip"
       elsif input == "descrip"
         puts "#{the_instrument.description}"
       elsif input == "cat"
         list_instruments
       elsif input == "exit"
-        next_time
+          next_time
       else
         puts "I don't understand your answer; please type a number for the instrument, cat, or exit"
-      end
     end
   end
   
   def next_time
     puts "See you next time and thanks for stopping by!"
+    end
   end
 end
 
