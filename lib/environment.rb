6c706b2498591a4f78648941297fafef0b48aa47
David Foote <david.a.foote@gmail.com>
2019-08-02 15:39:34 +0000

    Removed the non-working gems

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,8 +1,7 @@
 require 'open-uri'
 require 'nokogiri'
 require 'pry'
-require 'justify'
-require 'word_wrap'
+
 
 require_relative "./musical_instruments/version"
 require_relative './musical_instruments/cli'
