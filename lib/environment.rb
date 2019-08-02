832721ce8a0bd0077cf321035c26c45cd97bb4eb
David Foote <david.a.foote@gmail.com>
2019-08-02 05:46:08 +0000

    Tried installing word_wrap, can't get it to work, either

--- lib/environment.rb
+++ lib/environment.rb
@@ -2,6 +2,7 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 require 'justify'
+require 'word_wrap'
 
 require_relative "./musical_instruments/version"
 require_relative './musical_instruments/cli'
