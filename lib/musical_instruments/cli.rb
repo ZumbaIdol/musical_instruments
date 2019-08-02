4c98ab7237cca73c162f8c5e5cd383a7eac6f623
David Foote <david.a.foote@gmail.com>
2019-08-02 04:10:24 +0000

    Making the cli cleaner with whitespacing

--- lib/musical_instruments/cli.rb
+++ lib/musical_instruments/cli.rb
@@ -10,31 +10,47 @@ class MusicalInstruments::CLI
   end
   
   def list_instruments
+    puts ""
     puts "Musical Instruments for sale:"
+    puts ""
     @instruments = MusicalInstruments::Instruments.cataolog
     @instruments.each.with_index(1) do |instrument, i|
+      puts ""
       puts "#{i}. #{instrument.name} - #{instrument.price}"
+      puts ""
     end
   end
   
   def catalog
     input = nil
     while input != "exit"
+      puts ""
       puts "Enter the number of the instrument you would like more info about, cat to see the catalog again, or type exit:"
+      puts ""
       input = gets.strip.downcase
       
       if input.to_i > 0
         the_instrument = @instruments[input.to_i - 1]
+        puts ""
         puts "#{the_instrument.name} - #{the_instrument.price}"
+        puts ""
         puts "For more info, type descrip"
+        puts ""
         elsif input == "descrip"
+          puts ""
+          puts "-----------Description------------"
+          puts ""
           puts "#{the_instrument.description}"
+          puts ""
         elsif input == "cat"
           list_instruments
         elsif input == "exit"
           next_time
+          binding.pry
       else
+        puts ""
         puts "I don't understand your answer..."
+        puts ""
     end
   end
   
